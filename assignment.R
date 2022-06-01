9e60d2dc085bbd4a88ea6771015bb64e9b93c7ea
stougmeister <dannystougjje@msn.com>
2022-05-31 21:52:41 -0700

    text created in R for assignment

---
+++ assignment.R
@@ -0,0 +1,11 @@
+print("This file was created for coursera datascience assignment")
+
+print("It was created within RStudio")
+
+print("And pushed on GitHub")
+
+print("the follwing text is created for the assignment")
+
+print("## This is a markdown file")
+
+print("## By StougMeister")
